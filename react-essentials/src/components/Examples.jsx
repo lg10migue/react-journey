00c4a262cfb5bb46d40b28976dedadad2594b1b4
Miguel Angel López Guzmán <lg10migue@gmail.com>
2026-02-06 22:59:12 -0700

    Refactor App component.
    Refactored the App component to simplify structure by removing state management and integrating CoreConcepts and Examples components directly.
    Added the Tabs component for managing tabbed navigation.
    Refactored TabButton component to spread props and remove the onClick prop.
    Added a Section component for structured content display.
    Added the Examples component with a tabbed interface for displaying topics.
    Added the CoreConcepts component to display the core concepts list.

---
+++ react-essentials/src/components/Examples.jsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import { EXAMPLES } from "../data";
+import TabButton from "./TabButton";
+import Section from "./Section";
+import Tabs from "./Tabs";
+
+export default function Examples() {
+  const [selectedTopic, setSelectedTopic] = useState();
+
+  function handleTabClick(selectedTab) {
+    setSelectedTopic(selectedTab);
+  }
+
+  let tabContent = <p>Please select a topic.</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
+  }
+
+  return (
+    <Section title="Examples" id="examples">
+      <Tabs
+        buttons={
+          <>
+            <TabButton
+              isSelected={selectedTopic === "components"}
+              onClick={() => handleTabClick("components")}
+            >
+              Components
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "jsx"}
+              onClick={() => handleTabClick("jsx")}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "props"}
+              onClick={() => handleTabClick("props")}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              isSelected={selectedTopic === "state"}
+              onClick={() => handleTabClick("state")}
+            >
+              State
+            </TabButton>
+          </>
+        }
+      >
+        {tabContent}
+      </Tabs>
+      <menu></menu>
+    </Section>
+  );
+}
